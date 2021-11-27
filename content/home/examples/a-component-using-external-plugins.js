5bb8505e356c3e66edd9dd169833023f22ebe253
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-11-27 14:27:44 +0530

    Update a-component-using-external-plugins.js

--- content/home/examples/a-component-using-external-plugins.js
+++ content/home/examples/a-component-using-external-plugins.js
@@ -17,16 +17,16 @@ class MarkdownEditor extends React.Component {
   render() {
     return (
       <div className="MarkdownEditor">
-        <h3>Input</h3>
+        <h3>ආදානය</h3>
         <label htmlFor="markdown-content">
-          Enter some markdown
+          markdown ටිකක් ඇතුළත් කරන්න
         </label>
         <textarea
           id="markdown-content"
           onChange={this.handleChange}
           defaultValue={this.state.value}
         />
-        <h3>Output</h3>
+        <h3>ප්‍රතිදානය</h3>
         <div
           className="content"
           dangerouslySetInnerHTML={this.getRawMarkup()}
